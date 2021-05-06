81f216ca9567d43f54d33f3c0959428f0bf35f3b
Carles <carlesrojas@outlook.com>
2021-05-06 15:54:17 +0200

    Added Service Worker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 
 import App from "App";
 import EventsPubSub from "EventsPubSub";
+import * as serviceWorker from "./serviceWorker";
 
 // Contexts
 import UtilsProvider from "contexts/Utils";
@@ -25,3 +26,6 @@ ReactDOM.render(
     </UtilsProvider>,
     document.getElementById("root")
 );
+
+// Register service worker
+serviceWorker.register();
